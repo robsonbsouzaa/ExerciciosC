a0d551536cd624a138f49c4eaf8854116ab94ce1
Robson Souza <156427878+robsonbsouzaa@users.noreply.github.com>
2025-05-09 14:06:58 -0300

    Add files via upload

---
+++ Teste/main.c
@@ -0,0 +1,20 @@
+//Bibliotecas
+#include <stdio.h>
+#include <locale.h>
+//Inicio
+int main(){
+  setlocale(LC_ALL,"");
+  char time[20];
+  int vitorias, empates, pontos;
+  system("color 1F");
+  printf("Digite o time: \n");
+  scanf("%s", time);
+  printf("Informe a quantidade de vitorias:\n");
+  scanf("%d",&vitorias);
+  printf("Informe a quantidade de empates:\n");
+  scanf("%d", &empates);
+  pontos = (vitorias*3) + empates;
+  printf("%s fez um total de %d pontos",time, pontos);
+  return 0;
+  
+}
